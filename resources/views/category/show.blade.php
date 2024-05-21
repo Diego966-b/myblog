828f7f4aff41b8f5478fe3656b6d6c795f06bf59
Diego966-b <diegobennjaminn@gmail.com>
2024-05-21 15:10:10 -0300

    Primer commit

---
+++ resources/views/category/show.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Category Show</title>
+</head>
+
+<body>
+  <p>
+    Vista detalle del post <?php echo print_r($_GET);?>
+  </p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('auth/', function () {
+  return view('auth/login');
+});
+
+Route::get('logout', function () {
+  return view('logout');
+});
+
+Route::get('category/', function () {
+  return view('category/index');
+});
+
+Route::get('category/show/{id}', function () {
+  return view('category/show');
+});
+
+Route::get('category/create/', function () {
+  return view('category/create');
+});
+
+Route::get('category/edit/{id}', function () {
+  return view('category/edit');
 });
